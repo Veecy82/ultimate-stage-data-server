f212a2b74eb83bb9492e72e924d6645551b3d880
Caleb Stromberg <calebstromberg@gmail.com>
2023-04-26 16:47:51 -0700

    Only show error stack trace on local build

--- app.js
+++ app.js
@@ -55,10 +55,11 @@ app.use(function (err, req, res, next) {
   }
 
   res.locals.message = err.message
+  err.stack = process.env.PORT ? undefined : err.stack
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   res.status(err.status || 500)
-  res.render('error')
+  res.render('error', { title: 'Error' })
 })
 
 module.exports = app
